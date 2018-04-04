7bb4b2c5ab32399addc6c710003d3ff0cfecb0a2
gagantopcoder <gagan_1315708_2018@yahoo.com>
2018-04-04 12:24:00 +0530

    Fourth Commit

---
+++ Application/app/src/main/java/com/example/gagan/application/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.gagan.application;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Context context = getApplicationContext();
+        CharSequence text = "Hey I M A Toast";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, "Hey I M A Toast", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+
+
+
+}
