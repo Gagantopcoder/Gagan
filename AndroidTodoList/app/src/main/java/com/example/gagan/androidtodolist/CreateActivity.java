ea4d52c0c56eca90a7689ac6b483508b53ceef4d
gagantopcoder <gagan_1315708_2018@yahoo.com>
2018-05-15 15:20:38 +0530

    15th commit

---
+++ AndroidTodoList/app/src/main/java/com/example/gagan/androidtodolist/CreateActivity.java
@@ -0,0 +1,15 @@
+package com.example.gagan.androidtodolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+        <RelativeLayout xmls:android="http://schemas.com/apk/res/android" xmls:tools="https://schemas.android.com/tools" android:layout_width="match_parent" android:layout_height="match_parent" android:paddingLeft="@dimen/activity"
+
+    }
+}
